0d8a2c2a8e37ebf55d5480541ff149a8d5c96c87
Neveric <neveric@protonmail.com>
2022-07-13 02:06:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,7 @@ import os
 a = os.environ.get('HELLO_KITTY', 'NO HELLO FOR YOU')
 if a == 'HELLO SNAKE':
     print('IT IS COOL SNAKE BTW')
+print(str(None) + ' aaf')
+dick = {'хуй': 'bar', 'hello': 'kitty'}
+print(dick.keys())
+print(list(dick.keys())[0])
